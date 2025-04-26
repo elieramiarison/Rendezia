7112a0a754096b92679a29bcd1aead7041d6f7e9
elieramiarison <eliespirale1@gmail.com>
2025-04-26 17:55:30 +0300

    Configuration de next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-// import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
