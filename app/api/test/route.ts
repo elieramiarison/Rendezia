6322a9514ce7a7ea618ede5151dfe4965ee52692
elieramiarison <eliespirale1@gmail.com>
2025-05-03 02:47:50 +0300

    teste19

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,16 +1,25 @@
-import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth/next";
+// import { NextResponse } from "next/server";
+// import { getServerSession } from "next-auth/next";
 // import { authOptions } from "@/lib/auth";
-import { authOptions } from "../../../lib/auth"
+// import { authOptions } from "../../../lib/auth"
 import { connectDB } from "../connection/mongodb";
 
 export async function GET() {
-    const session = await getServerSession(authOptions);
-    await connectDB()
+    // const session = await getServerSession(authOptions);
+    // await connectDB()
 
-    if (!session) {
-        return NextResponse.json({ error: "Utilisateur non connecté", session: null }, { status: 401 });
+    try {
+        await connectDB();
+        console.log('MongoDB connected ✅')
+        return new Response('MongoDB connected ✅');
+    } catch (error) {
+        console.log('MongoDB connection error ❌')
+        return new Response('MongoDB connection error ❌', { status: 500 });
     }
 
-    return NextResponse.json({ message: "Utilisateur connecté", session });
+    // if (!session) {
+    //     return NextResponse.json({ error: "Utilisateur non connecté", session: null }, { status: 401 });
+    // }
+
+    // return NextResponse.json({ message: "Utilisateur connecté", session });
 }
