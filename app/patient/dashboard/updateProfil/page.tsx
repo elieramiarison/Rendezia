643174de5f5e1a7ac69db287ad284a95994c6019
elieramiarison <eliespirale1@gmail.com>
2025-05-08 01:57:45 +0300

    modification session5

--- app/patient/dashboard/updateProfil/page.tsx
+++ app/patient/dashboard/updateProfil/page.tsx
@@ -79,13 +79,13 @@ export default function UpdateProfil() {
             });
             setPreviewImage(session.user.image || "");
         }
-        // const handleFocus = () => {
-        //     // Forcer une refetch de la session quand l’onglet devient actif
-        //     window.location.reload();
-        // };
+        const handleFocus = () => {
+            // Forcer une refetch de la session quand l’onglet devient actif
+            window.location.reload();
+        };
 
-        // window.addEventListener("focus", handleFocus);
-        // return () => window.removeEventListener("focus", handleFocus);
+        window.addEventListener("focus", handleFocus);
+        return () => window.removeEventListener("focus", handleFocus);
     }, [session, status])
 
     if (status === "loading") {
